3790a722930a968aa6bb8ff13ebe42e475055b22
Luan1611 <luan_1611@hotmail.com>
2024-09-03 18:55:03 -0300

    feat: css properties, data for test, cards of available services

---
+++ list.js
@@ -0,0 +1,78 @@
+
+const availableServices = [
+
+    {
+        'serviceDate': '03/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 2},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 1},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 1}
+        ]
+    },
+
+    {
+        'serviceDate': '05/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 1},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 2},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 1}
+        ]
+    },
+
+    {
+        'serviceDate': '09/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 0},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 1},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 2}
+        ]
+    },
+
+    {
+        'serviceDate': '13/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 3},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 1},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 1}
+        ]
+    },
+
+    {
+        'serviceDate': '15/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 1},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 3},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 0}
+        ]
+    },
+
+    {
+        'serviceDate': '19/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 0},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 0},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 0}
+        ]
+    },
+
+    {
+        'serviceDate': '21/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 1},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 3},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 2}
+        ]
+    },
+
+    {
+        'serviceDate': '26/08/2024',
+        'services': [
+            {'serviceType': 'Guincho Local', 'serviceQuantity': 0},
+            {'serviceType': 'Guincho Intermunicipal', 'serviceQuantity': 0},
+            {'serviceType': 'Guincho Interestadual', 'serviceQuantity': 0}
+        ]
+    }
+
+]
+
+export default availableServices;
